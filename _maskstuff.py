fixed the ROI masking for volume anaysis

_maskstuff.py
@@ -121,49 +121,58 @@ def maskROI(self, area='V1', atlas='benson', doV123=False, forcePath=False):
         self._areaJsons    = []
 
         hs = ['L', 'R'] if self._hemis == '' else [self._hemis]
-        for ar in self._area:
-            for at in self._atlas:
+        for at in self._atlas:
+            for ar in self._area:
                 for h in hs:
                     try:
                         areaJson =  [j for j in self._allAreaFiles if f'hemi-{h.upper()}' in path.basename(j) and
                                                                       f'desc-{ar}-' in path.basename(j) and
                                                                       at in path.basename(j)][0]
-                        with open(areaJson, 'r') as fl:
-                            maskinfo = json.load(fl)
+                    except:
+                        continue
+                    
+                    with open(areaJson, 'r') as fl:
+                        maskinfo = json.load(fl)
 
-                        self._areaJsons.append(maskinfo)
+                    self._areaJsons.append(maskinfo)
 
-                        if len(self._roiWhichHemi) == 0:
-                            doubleMask = np.ones(len(maskinfo['roiIndBold']))
+                    if len(self._roiWhichHemi) == 0:
+                        doubleMask = np.ones(len(maskinfo['roiIndBold']))
+                    else:
+                        if self._analysisSpace == 'volume':
+                            doubleMask = np.invert(np.any([np.all(i==self._roiIndOrig[self._roiWhichHemi == h], axis=1) 
+                                        for i in maskinfo[roiIndOrigName]], axis=1))
                         else:
                             doubleMask = np.array([i not in self._roiIndOrig[self._roiWhichHemi == h]
-                                                   for i in maskinfo[roiIndOrigName]])
-                        doubleMask = doubleMask.astype(bool)
+                                                    for i in maskinfo[roiIndOrigName]])
+                    doubleMask = doubleMask.astype(bool)
 
-                        if h == 'L':
-                            self._roiIndBold = np.hstack((self._roiIndBold,
-                                                          np.array(maskinfo['roiIndBold'])[doubleMask]))
-                            lHemiSize = maskinfo['thisHemiSize']
-                        elif h == 'R':
-                            self._roiIndBold = np.hstack((self._roiIndBold,
-                                                          np.array(maskinfo['roiIndBold'])[doubleMask] + lHemiSize))
+                    if h == 'L':
+                        self._roiIndBold = np.hstack((self._roiIndBold,
+                                                        np.array(maskinfo['roiIndBold'])[doubleMask]))
+                        lHemiSize = maskinfo['thisHemiSize']
+                    elif h == 'R':
+                        self._roiIndBold = np.hstack((self._roiIndBold,
+                                                        np.array(maskinfo['roiIndBold'])[doubleMask] + lHemiSize))
 
-                        self._roiIndOrig = np.vstack((self._roiIndOrig,
-                                                          np.array(maskinfo[roiIndOrigName])[doubleMask]))
+                    if self._analysisSpace == 'volume':
+                        self._roiIndOrig = np.vstack((self._roiIndOrig, 
+                                                        np.array(maskinfo[roiIndOrigName])[doubleMask]))
+                    else:
+                        self._roiIndOrig = np.hstack((self._roiIndOrig, 
+                                                        np.array(maskinfo[roiIndOrigName])[doubleMask]))
+                        
 
-                        self._roiWhichHemi   = np.hstack((self._roiWhichHemi,
-                                                          np.tile(h,  sum(doubleMask))))
+                    self._roiWhichHemi   = np.hstack((self._roiWhichHemi,
+                                                        np.tile(h,  sum(doubleMask))))
 
-                        self._roiWhichArea   = np.hstack((self._roiWhichArea,
-                                                          np.tile(ar, sum(doubleMask))))
+                    self._roiWhichArea   = np.hstack((self._roiWhichArea,
+                                                        np.tile(ar, sum(doubleMask))))
 
-                        if h == 'L':
-                            self._roiMsk[maskinfo['roiIndBold']] = 1
-                        elif h == 'R':
-                            self._roiMsk[np.array(maskinfo['roiIndBold']) + lHemiSize] = 1
-
-                    except:
-                        pass
+                    if h == 'L':
+                        self._roiMsk[maskinfo['roiIndBold']] = 1
+                    elif h == 'R':
+                        self._roiMsk[np.array(maskinfo['roiIndBold']) + lHemiSize] = 1
 
         self._isROIMasked = 1
 

_plotstuff.py
@@ -6,17 +6,17 @@ Created on Tue Aug 30 10:18:56 2022
 @author: dlinhardt
 """
 
+import json
+import os
+from copy import deepcopy
+from glob import glob
+from os import path
+
+import matplotlib.pyplot as plt
+import nibabel as nib
 import numpy as np
 import scipy.stats as st
-import os
-import nibabel as nib
-import matplotlib.pyplot as plt
-# from scipy.io import loadmat
-from os import path
-from copy import deepcopy
 from PIL import Image
-from glob import glob
-import json
 
 try:
     from mayavi import mlab
@@ -43,9 +43,8 @@ class label():
 
         self.vertices = np.array(maskinfo['roiIndOrig'])
 
+
 #----------------------------------------------------------------------------#
-
-
 def _createmask(self, shape, otherRratio=None):
     # calculate and plot the coverage maps
 
@@ -240,7 +239,7 @@ def _get_surfaceSavePath(self, param, hemi, surface='cortex'):
 
     savePathB = path.join(self._baseP, self._study, 'derivatives', 'prfresult',
                           self._prfanaAn, 'cortex', self.subject, self.session)
-    ending = 'sphere' if surface == 'sphere' else 'cortex'
+    ending = surface
     areaStr = 'multipleAreas' if len(self._area) > 10 else "".join(self._area)
 
     savePathF = f'{self.subject}_{self.session}_{self._task}_{self._run}_hemi-{hemi[0].upper()}_desc-{areaStr}{VEstr}{Bstr}{Pstr}_{ending}'